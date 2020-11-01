4f2a2016cbc9ad56d9480e224df3a1cf94d98f99
alex-hagen <alex.hagen@rambler.ru>
2020-11-01 18:22:33 +0200

    Init

---
+++ __tests__/index.test.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@jest/globals';
+import reverse from '../src/index.js';
+
+
+test('reverse', () => {
+  expect(reverse('hello')).toEqual('olleh');
+  expect(reverse('')).toEqual('');
+});
